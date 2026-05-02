4cf51721c280641b6d25e995e0dffde518766943
Sanha258 <alwayscleanwm@gmail.com>
2026-05-02 13:45:25 -0300

    feat: setup Node + Express backend structure e docker configurado

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import healthRoutes from "./routes/health.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 👇 ESSA LINHA É A QUE FALTA
+app.use("/health", healthRoutes);
+
+export default app;
